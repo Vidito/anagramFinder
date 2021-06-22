3a3940d49d06b5f8adde8f25001557d9776d0d31
Vahid <56190096+Vidito@users.noreply.github.com>
2021-06-22 13:01:27 +0200

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,19 @@
+import json
+from collections import Counter
+
+data = json.load(open('dictionary_compact.json'))
+user = input('What is the word?')
+new = []
+
+for i in data:
+    if Counter(user) == Counter(i) and i != user:
+        new.append(i)
+    else:
+        continue
+
+if len(new) > 0:
+    print(f"Anagrams for {user} : {new} ")
+else:
+    print('Sorry, no anagrams for this word!')
+
+
